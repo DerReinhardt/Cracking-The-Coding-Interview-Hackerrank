c692de46594323a3b9994ff121ffd63fa0fc09e5
DerReinhardt <demon.sesshoumaru@gmail.com>
2016-10-07 11:28:18 -0700

    Update ctci davis' staircase_hackerrank.cpp

--- ctci davis' staircase_hackerrank.cpp
+++ ctci davis' staircase_hackerrank.cpp
@@ -23,6 +23,14 @@
 
 using namespace std;
 
+/* DP: From the position i that you are on, use the steps to jump ahead and count the accumulated steps.
+There is only one way to jump from position 0, hence ways[0] = 1
+From the position 0 jump to 0 + 1 and add the 1 from position 0 to the position 0 + 1. Repeat for steps 2, and 3.
+Now, from the position 1 you will have accumulated the ways from the previous position which is 0. Count the jumps ahead too.
+It is easy to see that from the i-th position we will have accumulated all the ways previous to i, and from i we can accumulate for
+the steps ahead.
+*/
+
 int countWays(int n){
 	const vector<int> steps = {1, 2, 3};
 	vector<int> ways(n + 1, 0);
